9743a344dfc1c2823d4ed8ad9a798b69b2de633d
Steve Richert <steve.richert@gmail.com>
2014-04-18 20:45:34 -0400

    Send test coverage reports to Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+if ENV["CODECLIMATE_REPO_TOKEN"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require "rspec/wait"
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |f| require f }
